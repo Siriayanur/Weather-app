0a9549378e1cf9a139312a485d2f71a4ab80660d
siriAyanur <meemusiri@gmail.com>
2020-07-31 17:02:19 +0530

    Initialised the first commit  Weather App

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+console.log('Client side Javascript')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const firstMessage = document.querySelector('#message-1')
+const secondMessage = document.querySelector('#message-2')
+
+
+
+weatherform.addEventListener('submit',(e) => {
+
+    const location = search.value
+
+    firstMessage.textContent='Loading...'
+    secondMessage.textContent = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            firstMessage.textContent = data.error;
+        } else{
+            secondMessage.textContent = data.forecast
+            firstMessage.textContent = data.location
+        }
+    })
+    
+})
+
+
+    e.preventDefault()
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request  = require('postman-request')
+
+const forecast = (latitude,longitude, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=cdbb06b2937ce3c05a74cbcdf6066c7d&query='+latitude+','+longitude+'&units=m'
+    request({url,json:true},(error,{body}) => {
+        if(error){
+            callback('Unable to fetch data. Check Connectivity',undefined)
+        } else if(body.error) {
+            callback("Parameters aren't correct. Check longitude and latitude",undefined)
+        } else {
+            callback(undefined,
+                { weather : body.current.weather_descriptions[0],
+                    temperature:body.current.temperature,
+                    feelslike:body.current.feelslike }
+                )
+        }
+    })
+}
+module.exports = forecast;
